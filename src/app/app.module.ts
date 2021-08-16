364dbae44b55aade95beef961058e26523b8f0e2
jin <jm12131415@hotmail.com>
2021-08-16 17:31:12 +0800

    adjust structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { AvatarComponent } from './common/avatar/avatar.component';
-import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
-import { FooterItemComponent } from './footer/footer-item/footer-item.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { FooterItemComponent } from './layout/footer/footer-item/footer-item.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { SidebarItemComponent } from './layout/sidebar/sidebar-item/sidebar-item.component';
+import { AvatarComponent } from './shared/components/avatar/avatar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent,
-    AvatarComponent,
-    SidebarItemComponent,
     FooterItemComponent,
+    SidebarComponent,
+    SidebarItemComponent,
+    AvatarComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [AvatarComponent],
+  exports: [],
 })
 export class AppModule {}
