8a369f64c38b8c166efb85198bbdfcd5cde0f83d
jin <jm12131415@hotmail.com>
2021-08-17 02:42:52 +0800

    update clsx to ngClass

--- src/app/layout/sidebar/sidebar-item/sidebar-item.component.ts
+++ src/app/layout/sidebar/sidebar-item/sidebar-item.component.ts
@@ -15,31 +15,5 @@ export class SidebarItemComponent implements OnInit {
     this.active = false;
   }
 
-  getMenuClass() {
-    return (
-      !this.separator &&
-      clsx(
-        'relative h-[48px] flex justify-start items-center rounded-r-[8px] cursor-pointer text-blueGray-400 font-bold text-[14px]',
-        {
-          group: !this.active,
-          'hover:bg-red-100': !this.active,
-          'bg-red-100': this.active,
-        }
-      )
-    );
-  }
-
-  getMenuBarClass() {
-    return (
-      !this.separator &&
-      clsx(
-        'absolute left-0 top-0 w-[6px] h-full bg-red-500 rounded-[10px] transform -translate-x-1/2',
-        {
-          'hidden group-hover:block': !this.active,
-        }
-      )
-    );
-  }
-
   ngOnInit(): void {}
 }
